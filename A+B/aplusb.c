4dc9c07b0da79a8b15acfbc8ff25ab389026b49b
coffee <1219774571@qq.com>
2018-08-12 15:29:19 +0800

    Update aplusb.c

--- A+B/aplusb.c
+++ A+B/aplusb.c
@@ -4,17 +4,12 @@
  * */
 int aplusb(int a,int b)
 {
-	int c;
-	if ((a & b) == 0)//说明是正数
-	{
-		return (a | b);
-	}
-	//其他是负数的可能
-	while ((a & b) != 0)
-	{
-		c = a ^ b;//获取他们的补位，就是除去进位的值
-		b = (a & b) << 1;//获取他们的进位，然后左移一位
-		a = c;
-	}
-	return (a | b);
+        int carry = a & b; //检测是否有进位
+        while (carry)
+        {
+            carry = (a & b) << 1; //处理进位
+            a = a ^ b; //去除进位的值,
+            b = carry;
+        }
+        return a | b;
 }
